ff9abdb7c07ba7b73b9f54910c4c81d0c9bb1162
Tom Benevides <eltonfelixbenevides@gmail.com>
2020-12-06 23:26:17 -0400

    testing the github action

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -24,7 +24,7 @@ return [
     // ----
 
     // Project name. Shown in the window title.
-    'project_name' => 'FAESPE - Sistema de Compras',
+    'project_name' => 'FAESPE - Gestão de Requisições',
 
     // When clicking on the admin panel's top-left logo/name,
     // where should the user be redirected?
