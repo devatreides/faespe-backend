1c710898d11d3eb030b8664857c48788bb737342
Tom Benevides <eltonfelixbenevides@gmail.com>
2020-12-06 23:31:28 -0400

    testing the github action - part 2

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -71,7 +71,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<b>FAESPE</b> SysCompra',
+    'project_logo'   => '<b>FAESPE</b> GestReq',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
